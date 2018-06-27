8b9657a2dd1841f4546057f61d8d8bcb48bfd1e0
Edimo Nkoe Pierre <pedimonkoe@yahoo.com>
2018-06-28 01:00:42 +0200

    initial commits

---
+++ component/Home.js
@@ -0,0 +1,21 @@
+import React , {Component } from 'react';   
+import {Text , View  } from 'react-native'; 
+import { Container, Header, Content, Footer, FooterTab, Button, Icon , Badge } from 'native-base';
+
+
+export default class Home extends Component {
+    render()
+    {
+        return (
+            <Container>
+           
+            <Content />
+              <Footer>
+                <FooterTab>
+               
+                </FooterTab>
+              </Footer>
+            </Container>
+        )
+    }
+}
---
+++ index.js
@@ -0,0 +1,7 @@
+import { AppRegistry } from 'react-native';
+import App from './App';
+
+import Home from './component/Home';
+
+
+AppRegistry.registerComponent('sociallearning', () => Home);
